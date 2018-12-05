f663a5418faa062289daee3f4f222ad3a4721223
megsnyder <42872006+megsnyder@users.noreply.github.com>
2018-12-05 11:31:24 -0500

    Updated from Brython Server: 12/5/2018 11:31:23 AM

--- FinalProgram.py
+++ FinalProgram.py
@@ -217,6 +217,8 @@ class Game(App):
     def left(self,event):
         for player in self.getSpritesbyClass(Player):
             player.vx = -2
+            player.width=-1*player.width
+            player.x=player.x+10
         for arm in self.getSpritesbyClass(Arm):
             arm.vx = -2
         for snowball in self.getSpritesbyClass(Snowball):
@@ -225,6 +227,7 @@ class Game(App):
     def left2(self,event):
         for player in self.getSpritesbyClass(Player):
             player.vx=0 
+            player.width=-1*player.width
         for arm in self.getSpritesbyClass(Arm):
             arm.vx = 0
         for snowball in self.getSpritesbyClass(Snowball):
